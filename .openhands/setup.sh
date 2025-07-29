b8173c054cacf225ecd5029620ae855efc15aab5
zoomer_k <ikoeg2000-efegrrg@yahoo.co.jp>
2025-07-29 12:02:29 +0900

    Update setup.sh

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo npm install --global tree-sitter-cli @ast-grep/cli
-sudo tree-sitter init-config
-sudo git clone --depth=1 https://github.com/gmlarumbe/tree-sitter-systemverilog /tmp/tree-sitter-systemverilog
-sudo cd /tmp/tree-sitter-systemverilog
-sudo tree-sitter generate -b --abi 14 --libdir .
+tree-sitter init-config
+git clone --depth=1 https://github.com/gmlarumbe/tree-sitter-systemverilog /tmp/tree-sitter-systemverilog
+pushd /tmp/tree-sitter-systemverilog
+tree-sitter generate -b --abi 14 --libdir .
+pushd
+sg test --skip-snapshot-tests
+
 
